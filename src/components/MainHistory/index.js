e2fba7851edcbb613bc60fa2276953f17ad18f18
Do Nhu Hoc <hocvodoi9898@gmail.com>
2023-06-12 16:04:20 +0700

    chinh sua

--- src/components/MainHistory/index.js
+++ src/components/MainHistory/index.js
@@ -1,9 +1,293 @@
-
+import "./style.css";
 
 const MainHistory = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <main className="app">
+      <div class="container-fluid text-center">
+        <h1 className="heading mb-3">Lịch sử</h1>
+        <div class="row box-table">
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>A Thuận + Việt</td>
+                  <td>A Koi + Sáng</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>A Thanh + Toản</td>
+                  <td>A Đạt 93 + Hoàng</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Đức Anh T3</td>
+                  <td>Long</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>A Bình + Hoàng (bạn Toản)</td>
+                  <td>A Zik + Học</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>A Đức + A Trường</td>
+                  <td>Tuấn + A Duy</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>Vũ + Đạt</td>
+                  <td>Sinh + Thành</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 6</h2>
+          </div>
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 7</h2>
+          </div>
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 8</h2>
+          </div>
+          
+          <div class="w-100"></div>
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 9</h2>
+          </div>
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 10</h2>
+          </div>
+          <div class="col">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Team 1</th>
+                  <th scope="col">Team 2</th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">4</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">5</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th scope="row">6</th>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                
+              </tbody>
+            </table>
+            <h2 className='month'>Tháng 11</h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MainHistory;
--- src/components/TeamDivision/index.js
+++ src/components/TeamDivision/index.js
@@ -1,11 +1,135 @@
-
+import "./style.css";
 
 const TeamDivision = () => {
-    return (
-        <> 
-        
-        </>
-    )
-}
+  
+  const team1 = []
+  const team2 = []
+  return (
+    <div className="container text-center">
+      {/* <h1 className="text-center">Chĩa Team V2.0</h1>
+      <h2 className="text-center" id="status">
+        Đang chĩa nhé...
+      </h2>
+      <div className="text-center">
+        <button onclick={init()} id="btnStart">
+          Start
+        </button>
+        <table id="chiateam">
+          <tr>
+            <th>Team 1</th>
+            <th>Team 2</th>
+          </tr>
+          <tbody id="tbodyTb"></tbody>
+        </table>
+        <div>
+          <h2 id="status_congrast">
+            Chúc mừng các đội, chúc các bạn đá bóng vui vẽ!
+          </h2>
+        </div>
+      </div> */}
+
+      <h2 className="heading">Nhập từng thành viên của 2 đội</h2>
+      
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Team 1</th>
+            <th scope="col">Team 2</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>
+              <input className="ip-name-1"/>
+            </td>
+            <td>
+              <input className="ip-name-2"/>
+            </td>
+            
+          </tr>
+        </tbody>
+      </table>
+      <button className="btn btn-primary btn-chia">
+        Chia
+      </button>
+
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Team 1</th>
+            <th scope="col">Team 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>
+              sdfsdf
+            </td>
+            <td>
+              fsdfsdf
+            </td>
+            
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default TeamDivision;
